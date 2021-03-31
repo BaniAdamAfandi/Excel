7530c79ec7547310eadf4fd118e8f24ecb2cc10c
bani adam afandi <45775790+BaniAdamAfandi@users.noreply.github.com>
2021-03-31 16:42:36 +0700

    Create skalarceiling.sql

---
+++ SQLUsingFUNCTIONandGROUPBY/skalarceiling.sql
@@ -0,0 +1,8 @@
+Fungsi Skalar Matematika - CEILING()
+Fungsi CEILING()
+
+Syntax: 
+SELECT StudentID, FirstName, LastName, CEILING(Semester1) as Semester1, CEILING(Semester2) as Semester2, MarkGrowth FROM students;	
+SELECT CEILING(ColumnName)  
+FROM TableName; 
+ 
