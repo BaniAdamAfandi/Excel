2f9b0a24e8cfe4f78c7753be43def15009687bcd
bani adam afandi <45775790+BaniAdamAfandi@users.noreply.github.com>
2021-03-31 16:53:31 +0700

    Create skalarSQRT.sql

---
+++ SQLUsingFUNCTIONandGROUPBY/skalarSQRT.sql
@@ -0,0 +1,7 @@
+Fungsi Skalar Matematika - SQRT( )
+Fungsi SQRT()
+
+Syntax: 
+SELECT StudentID, FirstName, LastName, SQRT(Semester1) as Semester1, Semester2, MarkGrowth FROM students;	
+SELECT SQRT(ColumnName)  
+FROM TableName; 
