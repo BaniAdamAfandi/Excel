b12d4ffbfae72eeb876f43cf727a3b2baafd2156
bani adam afandi <45775790+BaniAdamAfandi@users.noreply.github.com>
2021-03-31 16:46:33 +0700

    Create sklarfloor.sql

---
+++ SQLUsingFUNCTIONandGROUPBY/sklarfloor.sql
@@ -0,0 +1,7 @@
+Fungsi Skalar Matematika - FLOOR()
+Fungsi FLOOR()
+
+Syntax: 
+SELECT StudentID, FirstName, LastName, FLOOR(Semester1) as Semester1, FLOOR(Semester2) as Semester2, MarkGrowth FROM students;	
+SELECT FLOOR(ColumnName)  
+FROM TableName; 
